d2a8723a3d437a954cdb845ec7df1f2c26b18ae7
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-04-28 14:19:17 +0800

    wip: add initial version of pmrspy

---
+++ examples/example.cpp
@@ -0,0 +1,55 @@
+#include "pmrspy/pmrspy.hpp"
+#include <array>
+
+template <typename Container, typename... Values>
+auto create_container(auto* resource, Values&&... values) {
+  Container result{resource};
+  result.reserve(sizeof...(values));
+  (result.emplace_back(std::forward<Values>(values)), ...);
+  return result;
+};
+
+int main() {
+  pmrspy::print_alloc default_alloc{"Rogue PMR Allocation!", std::pmr::null_memory_resource()};
+  std::pmr::set_default_resource(&default_alloc);
+
+  pmrspy::print_alloc oom{"Out of Memory", std::pmr::null_memory_resource()};
+
+  std::array<std::uint8_t, 32768> buffer{};
+  std::pmr::monotonic_buffer_resource
+    underlying_bytes(buffer.data(), buffer.size(), &oom);
+
+  pmrspy::print_alloc monotonic{"Monotonic Array", &underlying_bytes};
+
+  std::pmr::unsynchronized_pool_resource unsync_pool(&monotonic);
+
+  pmrspy::print_alloc pool("Pool", &unsync_pool);
+
+  for (auto i = 0uz; i < 10; ++i)
+  {
+    fmt::print("Starting Loop Iteration\n");
+    auto vec = create_container<std::pmr::vector<std::pmr::string>>(
+        &pool, "Hello", "World", "Hello Long String", "Another Long String");
+    fmt::print("Emplacing Long String\n");
+    vec.emplace_back("a different long string");
+    fmt::print("Emplacing Long String\n");
+    vec.emplace_back("a different long string 1");
+    fmt::print("Emplacing Long String\n");
+    vec.emplace_back("a different long string");
+    fmt::print("Emplacing Long String\n");
+    vec.emplace_back("a different long string");
+    fmt::print("Emplacing Short String\n");
+    vec.emplace_back("bob");
+    fmt::print("Emplacing Short String\n");
+    vec.emplace_back("was");
+    fmt::print("Erasing First Element\n");
+    vec.erase(vec.begin());
+    fmt::print("Erasing First Element\n");
+    vec.erase(vec.begin());
+    fmt::print("Erasing First Element\n");
+    vec.erase(vec.begin());
+    fmt::print("Finishing Loop Iteration\n");
+  }
+
+  fmt::print("Exiting Main");
+}
---
+++ include/pmrspy/pmrspy.hpp
@@ -0,0 +1,105 @@
+#include <cctype>
+#include <cstddef>
+#include <memory_resource>
+#include <string>
+#include <cassert>
+#include <fmt/core.h>
+#include <fmt/color.h>
+
+namespace pmrspy
+{
+  class print_alloc : public std::pmr::memory_resource
+  {
+  public:
+    print_alloc(const std::string name, std::pmr::memory_resource* upstream)
+      : name_(std::move(name))
+      , upstream_(upstream)
+    {
+      assert(upstream);
+    }
+
+  private:
+    std::string name_;
+    std::pmr::memory_resource* upstream_;
+
+    void* do_allocate(std::size_t bytes, std::size_t alignment) override
+    {
+      fmt::print(fg(fmt::color::light_green), "[{} (alloc)] Size: {} Alignment: {} ...\n", name_, bytes, alignment);
+      auto result = upstream_->allocate(bytes, alignment);
+      return result;
+    }
+
+    std::string format_destroyed_bytes(std::byte* p, const std::size_t size)
+    {
+      std::string result = "";
+      bool in_string = false;
+
+      auto format_char = [](bool& in_string, const char c, const char next)
+      {
+        auto format_byte = [](const char byte)
+        {
+          return fmt::format(" {:02x}", static_cast<unsigned char>(byte));
+        };
+
+        if (std::isprint(static_cast<int>(c)))
+        {
+          if (!in_string)
+          {
+            if (std::isprint(static_cast<int>(next)))
+            {
+              in_string = true;
+              return fmt::format(" \"{}", c);
+            }
+            else
+            {
+              return format_byte(c);
+            }
+          }
+          else
+          {
+            return std::string(1, c);
+          }
+        }
+        else
+        {
+          if (in_string)
+          {
+            in_string = false;
+            return '"' + format_byte(c);
+          }
+          return format_byte(c);
+        }
+      };
+
+      std::size_t pos = 0;
+      for (; pos < std::min(size - 1, static_cast<std::size_t>(32)); ++pos)
+      {
+        result += format_char(in_string, static_cast<char>(p[pos]),
+          static_cast<char>(p[pos + 1]));
+      }
+      result += format_char(in_string, static_cast<char>(p[pos]), 0);
+      if (in_string)
+      {
+        result += '"';
+      }
+      if (pos < (size - 1))
+      {
+        result += " <truncated...>";
+      }
+      return result;
+    }
+
+    void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override
+    {
+      fmt::print(fg(fmt::color::light_salmon), "[{} (dealloc)] Address: {} Dealloc Size: {} Alignment: {} Data: {}\n",
+        name_, p, bytes, alignment, format_destroyed_bytes(static_cast<std::byte*>(p), bytes)
+      );
+      upstream_->deallocate(p, bytes, alignment);
+    }
+
+    bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override
+    {
+      return this == &other;
+    }
+  };
+} // pmrspy
